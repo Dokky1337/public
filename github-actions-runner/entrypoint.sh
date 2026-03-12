4b67d9f7925b8a97aac19ab7c6a6ece4baec56a9
Dokky1337 <fredrik.dokken@hotmail.com>
2026-03-12 20:55:27 +0100

    Update entrypoint.sh

--- github-actions-runner/entrypoint.sh
+++ github-actions-runner/entrypoint.sh
@@ -4,7 +4,7 @@
 REGISTRATION_TOKEN="$(curl -X POST -fsSL \
   -H 'Accept: application/vnd.github.v3+json' \
   -H "Authorization: Bearer $GITHUB_PAT" \
-  -H 'X-GitHub-Api-Version: 2022-11-28' \
+  -H 'X-GitHub-Api-Version: 2026-03-10' \
   "$REGISTRATION_TOKEN_API_URL" \
   | jq -r '.token')"
 
